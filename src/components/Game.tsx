62a215e4963d46a2c4775ced4a52bb7b05c4ee41
kasoutsuuka <34936534+kasoutsuuka@users.noreply.github.com>
2022-07-23 09:56:37 +0900

    Update Game.tsx

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -194,7 +194,7 @@ const Game: React.FC<{
     let {
       data: { record },
     } = await axios({
-      url: "https://api.jsonbin.io/v3/b/62824f03019db46796a1252a/latest",
+      url: "https://api.jsonbin.io/v3/b/62db27842c868775a533c832/latest",
       method: "get",
       headers: {
         "X-Master-Key": masterKey,
